910e3f92a3b59b419a1afc1409fb19d31de97844
jtoonyspe <jtoony@spe.org>
2016-03-03 10:00:26 -0600

    JD's first commit from PyCharm

--- mainsite/context_processors/spe_login.py
+++ mainsite/context_processors/spe_login.py
@@ -1,47 +1,48 @@
 from django.conf import settings
 
+
 # A context processor to add the spe login cookie information to the current Context
 #   primarily used in header for every page to determine logged in status, url, and welcome message
 def set_login_values(request):
+    # get the environment and debug variables from the config file
+    #  NOTE: add to server side variables and check for future requests
+    spe_environment = settings.ENVIRONMENT
+    spe_debug = getattr(settings, "DEBUG", False)
 
-        # get the environment and debug variables from the config file
-        #  NOTE: add to server side variables and check for future requests
-        spe_environment = settings.ENVIRONMENT
-        spe_debug = getattr(settings, "DEBUG", False)
+    # set the boolean authenticated variable
+    spe_cookie_erights = request.COOKIES.get('ERIGHTS', '')
+    spe_cookie_constit = request.COOKIES.get('sm_constitid', '')
+    spe_authenticated = (spe_cookie_erights != '' and spe_cookie_constit != '')
+    # build out the name for the welcome message
+    spe_cookie_first = request.COOKIES.get('first_name', '')
+    spe_cookie_last = request.COOKIES.get('last_name', '')
+    spe_name = ''
+    if spe_cookie_first != '':
+        spe_name = spe_cookie_first + " "
+    spe_name += spe_cookie_last
 
-        # set the boolean authenticated variable
-        spe_cookie_erights = request.COOKIES.get('ERIGHTS', '')
-        spe_cookie_constit = request.COOKIES.get('sm_constitid','')
-        spe_authenticated = (spe_cookie_erights != '' and spe_cookie_constit != '')
-        # build out the name for the welcome message
-        spe_cookie_first = request.COOKIES.get('first_name','')
-        spe_cookie_last = request.COOKIES.get('last_name','')
-        spe_name = ''
-        if spe_cookie_first != '':
-            spe_name = spe_cookie_first + " "
-        spe_name += spe_cookie_last
+    if spe_authenticated:
+        spe_command = 'logout'
+        spe_label = 'Sign Out'
+    else:
+        spe_command = 'login'
+        spe_label = 'Sign In'
 
-        if spe_authenticated:
-            spe_command = 'logout'
-            spe_label = 'Sign Out'
-        else:
-            spe_command = 'login'
-            spe_label = 'Sign In'
+    # if our host is localhost or 127.0.0.1 then lets use our login that sets cookies specifically for testing
+    # otherwise we route to erights
+    if request.META['REMOTE_ADDR'] == '127.0.0.1':
+        spe_url = "/localhost/" + spe_command + "/"
+    else:
+        # build out the erights url for if we are not localhost
+        spe_target_url = request.get_full_path()
+        spe_url = "/appssecured/login/servlet/ErightsLoginServlet?g=ci&command=" + spe_command + "&ERIGHTS_TARGET=" + \
+                  spe_target_url
 
-        # if our host is localhost or 127.0.0.1 then lets use our login that sets cookies specifically for testing otherwise we route to erights
-        if request.META['REMOTE_ADDR'] == '127.0.0.1':
-            spe_url = "/localhost/" + spe_command + "/"
-        else:
-            # build out the erights url for if we are not localhost
-            spe_target_url = request.get_full_path()
-            spe_url = "/appssecured/login/servlet/ErightsLoginServlet?g=ci&command=" + spe_command + "&ERIGHTS_TARGET=" + spe_target_url
-
-        return {
-            'spe_authenticated': spe_authenticated,
-            'spe_name': spe_name,
-            'spe_url': spe_url,
-            'spe_label': spe_label,
-            'spe_environment': spe_environment,
-            'spe_debug': spe_debug
-        }
-
+    return {
+        'spe_authenticated': spe_authenticated,
+        'spe_name': spe_name,
+        'spe_url': spe_url,
+        'spe_label': spe_label,
+        'spe_environment': spe_environment,
+        'spe_debug': spe_debug
+    }
