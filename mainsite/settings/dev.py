9d6a4b88000232ff95f10e82847789e2ebfef211
bodenmemberships <jboden@jboden-mac.local>
2016-03-06 08:38:15 -0600

    Moved DATABASE settings to dev and prod settings files.
    To allow for different databases in dev and production, move the the
    DATABASE settings to the individual dev and production settings files.

--- mainsite/settings/dev.py
+++ mainsite/settings/dev.py
@@ -2,5 +2,18 @@ from __future__ import absolute_import # optional, but I like it
 from .common import *
 
 # Development overrides
+
 DEBUG = True
 ENVIRONMENT = "DEV"
+
+DATABASES = {
+    'default': {
+        'CONN_MAX_AGE': 0,
+        'ENGINE': 'django.db.backends.sqlite3',
+        'HOST': 'localhost',
+        'NAME': 'project.db',
+        'PASSWORD': '',
+        'PORT': '',
+        'USER': ''
+    }
+}
--- mainsite/settings/production.py
+++ mainsite/settings/production.py
@@ -4,3 +4,15 @@ from .common import *
 # Development overrides
 DEBUG = False
 ENVIRONMENT = "PROD"
+
+DATABASES = {
+    'default': {
+        'CONN_MAX_AGE': 0,
+        'ENGINE': 'django.db.backends.sqlite3',
+        'HOST': 'localhost',
+        'NAME': 'project.db',
+        'PASSWORD': '',
+        'PORT': '',
+        'USER': ''
+    }
+}
