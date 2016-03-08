256af4147e06027df0c8afd32aa161bf58c42e68
Steve Stacha <sstacha@gmail.com>
2016-03-07 21:36:22 -0600

    changed the db path and removed the unneeded import that was causing mike problems

--- mainsite/settings/dev.py
+++ mainsite/settings/dev.py
@@ -1,4 +1,4 @@
-from __future__ import absolute_import # optional, but I like it
+#from __future__ import absolute_import # optional, but I like it
 from .common import *
 
 # Development overrides
@@ -23,7 +23,7 @@ DATABASES = {
         'CONN_MAX_AGE': 0,
         'ENGINE': 'django.db.backends.sqlite3',
         'HOST': 'localhost',
-        'NAME': 'project.db',
+        'NAME': os.path.join(PROJECT_DIR, 'project.db'),
         'PASSWORD': '',
         'PORT': '',
         'USER': ''
