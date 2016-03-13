aa011a28c94e80e42a395bd8ade66fc89c8dc42f
bodenmemberships <jboden@jboden-mac.local>
2016-03-13 10:48:56 -0500

    Cleaned up Article Admin Page
    Allowed collapse and expand of:
    Picture
    Free

--- spe_blog/admin.py
+++ spe_blog/admin.py
@@ -1,4 +1,8 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
 from django.contrib import admin
+from django.utils.translation import ugettext_lazy as _
 
 from .models import Article, Category, Publication, Issue
 from mainsite.models import Tier1Discipline
@@ -8,6 +12,29 @@ class ArticleAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",)}
     exclude = ['auto_tags']
     filter_horizontal = ('topics', )
+    fieldsets = (
+        (None, {
+            'fields': (
+                'publication', 'print_volume', 'print_issue', 'sponsored', 'category', 'title', 'slug', 'teaser', 'author', 'introduction', 'article_text', 'date', 'disciplines', 'topics', 'tags'
+            ),
+        }),
+        (_('Image'), {
+            'classes': ('collapse',),
+            'fields': (
+                'picture',
+                'picture_alternate',
+                'picture_attribution',
+                'picture_caption',
+            ),
+        }),
+        (_('Free'), {
+            'classes': ('collapse',),
+            'fields': (
+                'free',
+                ('free_start', 'free_stop'),
+            ),
+        }),
+    )
 
 #    def get_form(self, request, obj=None, **kwargs):
 #        if obj and obj.free:
