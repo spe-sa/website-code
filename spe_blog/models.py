c02ae9edd0d523e17dd0a84a67279ce0608cc2f9
Steve Stacha <sstacha@gmail.com>
2016-03-15 13:42:23 -0500

    removing the articles copy since there are no articles on the model to copy

--- spe_blog/models.py
+++ spe_blog/models.py
@@ -199,7 +199,7 @@ class EditorialPlugin(CMSPlugin):
 
     def copy_relations(self, old_instance):
         self.editorial = old_instance.editorial.all()
-        self.articles = old_instance.articles.all()
+ #       self.articles = old_instance.articles.all()
 
 # class ArticlePlugin(CMSPlugin):
 #     template = models.CharField(max_length=255, choices=PLUGIN_TEMPLATES, default=DEFAULT_PLUGIN_TEMPLATE)
