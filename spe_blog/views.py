3dcbf6a71dfca5ff6a6aa5722a92ff387bbee087
bodenmemberships <jboden@jboden-mac.local>
2016-03-14 21:15:08 -0500

    Add subscribe Block to Article Detail Page and Issues Page
    Sorted Issues by Date
    Added Last Issue Picture and Subscribe buttons to both Issues and
    Article Detail pages

--- spe_blog/views.py
+++ spe_blog/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import get_object_or_404, render
 from django.utils import timezone
 import sys
 
-from .models import Article, Issue
+from .models import Article, Issue, Publication
 
 
 def index(request):
@@ -16,11 +16,14 @@ def detail(request, article_id):
     q.article_hits += 1
     q.article_last_viewed = timezone.now()
     q.save()
-    return render(request, 'spe_blog/detail.html', {'article': q})
+    i = Issue.objects.filter(publication=q.publication).order_by('-date')[:1]
+    return render(request, 'spe_blog/detail.html', {'article': q, 'issues': i})
 
 
 def issue(request, publication_code):
-    sys.stderr.write("publication_code: " + publication_code + "\n")
-    issues = Issue.objects.filter(publication__code=publication_code.upper())
-    context = {'issues': issues, 'publication_code': publication_code}
+    #sys.stderr.write("publication_code: " + publication_code + "\n")
+    issues = Issue.objects.filter(publication__code=publication_code.upper()).order_by('-date')
+    pub = get_object_or_404(Publication, code=publication_code.upper())
+    context = {'issues': issues, 'publication_code': publication_code, 'show_subscribe_url': pub.subscription_url}
+    
     return render(request, 'spe_blog/issues.html', context)
