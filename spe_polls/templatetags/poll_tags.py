dc5bcfa031a265373a989868e69ec89d1759859a
bodenmemberships <jboden@jboden-mac.local>
2016-03-16 23:36:03 -0500

    Adding Tag Template for Form in SPE_Polls

--- spe_polls/templatetags/poll_tags.py
+++ spe_polls/templatetags/poll_tags.py
@@ -1,9 +1,15 @@
 from django import template
 from math import ceil
 
+#from .forms import x
+
 register = template.Library()
 
 
 @register.filter()
 def in_compare_to(value, total):
     return str(int(ceil(float(value) / float(total) * 100)))
+
+#@register.inclusion_tag('cms/plugins/poll_form.html')
+#def poll_form():
+#    return {'form': x(), 'action': '/some/url'}
