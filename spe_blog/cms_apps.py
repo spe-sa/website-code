d98f923b138929e021b5dbdd74857eeaad7c3457
bodenmemberships <jboden@jboden-mac.local>
2016-03-18 09:52:46 -0500

    Eliminated SPE_Blog App Hook

--- spe_blog/cms_apps.py
+++ spe_blog/cms_apps.py
@@ -1,9 +1,9 @@
-from cms.app_base import CMSApp
-from cms.apphook_pool import apphook_pool
-from django.utils.translation import ugettext_lazy as _
+#from cms.app_base import CMSApp
+#from cms.apphook_pool import apphook_pool
+#from django.utils.translation import ugettext_lazy as _
 
-class PublicationsApphook(CMSApp):
-    name = _("Publications")
-    urls = ["spe_blog.urls"]
+#class PublicationsApphook(CMSApp):
+#    name = _("Publications")
+#    urls = ["spe_blog.urls"]
 
-apphook_pool.register(PublicationsApphook)
+#apphook_pool.register(PublicationsApphook)
