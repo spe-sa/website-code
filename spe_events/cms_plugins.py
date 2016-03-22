e2771cba7c341ce95179ffe169880b3035e30c75
bodenmemberships <jboden@jboden-mac.local>
2016-03-22 10:30:43 -0500

    Events by Location Near You Plugin - Settings
    added ability to configure the personalization server via the settings
    file with local default (dev)

--- spe_events/cms_plugins.py
+++ spe_events/cms_plugins.py
@@ -1,3 +1,5 @@
+import requests
+import json
 from cms.plugin_base import CMSPluginBase
 from cms.plugin_pool import plugin_pool
 
@@ -6,6 +8,7 @@ from django.utils.translation import ugettext_lazy as _
 from django.contrib.gis.geoip import GeoIP
 
 from .models import EventsByCurrentIPPlugin
+from .settings import EVENT_PERSONALIZATION_SERVER
 
 class ShowEventsByCurrentLocationPluginPlugin(CMSPluginBase):
     model = EventsByCurrentIPPlugin
@@ -23,19 +26,19 @@ class ShowEventsByCurrentLocationPluginPlugin(CMSPluginBase):
         ip = '192.152.183.2'
         if ip:
             loc = g.city(ip)
-            req_str = 'http://iisdev1/iappsint/p13ndemo/api/I2KTaxonomy/GetEventList3?latitude=' + str(loc['latitude']) + '&longitude=' + str(loc['longitude']) + "&num=" + str(instance.number) + "&numKm=" + str(instance.radius)
+            req_str = EVENT_PERSONALIZATION_SERVER + '?latitude=' + str(loc['latitude']) + '&longitude=' + str(loc['longitude']) + "&num=" + str(instance.number) + "&numKm=" + str(instance.radius)
             req_str = req_str + "&discipline="
             for discipline in instance.disciplines.all():
                 req_str = req_str + discipline.eva_code
             req_str = req_str + "&eventtype="
             for type in instance.types.all():
                 req_str = req_str + type.name + ','
-            headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
-            try:
-                r = requests.get(req_str, headers=headers)
-                context.update({'meetings': r.json()})
-            except:
-                pass
+            headers = {'Accept': 'application/json'}
+            #try:
+            r = requests.get(req_str, headers=headers)
+            context.update({'events': r.json()})
+            #except:
+            #    pass
         else:
             loc = None
         context.update({'location': loc})
---
+++ spe_events/settings.py
@@ -0,0 +1,3 @@
+from django.conf import settings
+
+EVENT_PERSONALIZATION_SERVER = getattr(settings, 'EVENT_PERSONALIZATION_SERVER', ('http://iisdev1/iappsint/p13ndemo/api/I2KTaxonomy/GetEventList3'))
