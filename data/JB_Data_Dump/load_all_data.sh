bc83f1570c9eba4004c4d21bb80bfe2531770cbe
Steve Stacha <sstacha@gmail.com>
2016-03-23 16:05:44 -0500

    cleaned up some bugs in requirements.txt, added install instructions to root with readme fixed load_all_data script and removed the bad one from the old JD directory

--- data/JB_Data_Dump/load_all_data.sh
+++ data/JB_Data_Dump/load_all_data.sh
@@ -3,11 +3,11 @@
 
 # source environment
 
-source ../../env/bin/activate
-../manage.py loaddata data/JD_Data_Dump/auth_users.json
-../manage.py loaddata data/JD_Data_Dump/mainsite.json
-../manage.py loaddata data/JD_Data_Dump/spe_links.json
-../manage.py loaddata data/JD_Data_Dump/spe_events.json
-../manage.py loaddata data/JD_Data_Dump/spe_polls.json
-../manage.py loaddata data/JD_Data_Dump/spe_blog.json
-../manage.py loaddata data/JD_Data_Dump/cms_export.json
+source ../env/bin/activate
+./manage.py loaddata data/JB_Data_Dump/auth_users.json
+./manage.py loaddata data/JB_Data_Dump/mainsite.json
+./manage.py loaddata data/JB_Data_Dump/spe_links.json
+./manage.py loaddata data/JB_Data_Dump/spe_events.json
+./manage.py loaddata data/JB_Data_Dump/spe_polls.json
+./manage.py loaddata data/JB_Data_Dump/spe_blog.json
+./manage.py loaddata data/JB_Data_Dump/cms_export.json
