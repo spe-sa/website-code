1ae035d0b734361ffe678c5c999053ec66575f8d
bodenmemberships <jboden@jboden-mac.local>
2016-03-23 09:47:20 -0500

    Update Article Get Absolute URL to handle internal CMS Pages

--- spe_blog/models.py
+++ spe_blog/models.py
@@ -72,7 +72,7 @@ class Publication(models.Model):
     code = models.CharField(max_length=3, primary_key=True)
     name = models.CharField(max_length=150, unique=True)
     subscription_url = models.URLField(verbose_name=u'Subscription URL', blank=True, null=True)
-    url = PageField(verbose_name = "URL for article detail page")
+    cms_url = PageField(verbose_name = "URL for article detail page")
     active = models.BooleanField(default=True)
 
     def __unicode__(self):
@@ -162,7 +162,8 @@ class Article(models.Model):
 
     def get_absolute_url(self):
         if self.publication.url:
-            url = self.publication.url + "?art=" + str(self.id)
+            page = page.objects.get(pk=self.publication.cms_url)
+            url = page.get_absolute_url() + "?art=" + str(self.id)
         else:
             url = reverse('detail', kwargs={'article_id': self.id})
         return url
