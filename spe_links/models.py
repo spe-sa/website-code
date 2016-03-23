d88961f31569b626a462b4d5516c343ef2906d0e
bodenmemberships <jboden@jboden-mac.local>
2016-03-23 10:57:13 -0500

    Move SPELinks to cms page or absolute URL optionally

--- spe_links/models.py
+++ spe_links/models.py
@@ -5,7 +5,6 @@ from django.utils import timezone
 from django.utils.encoding import python_2_unicode_compatible
 
 
-# Create your models here.
 @python_2_unicode_compatible  # only if you need to support Python 2
 class SpeLinkCategory(models.Model):
     code = models.CharField(max_length=20)
@@ -13,7 +12,13 @@ class SpeLinkCategory(models.Model):
     # Adding class name; if present will be used instead of the name lower case
     class_name = models.CharField(max_length=255, blank=True, null=True)
     # Adding category url if present will show all link to the url location
-    url = models.URLField(max_length=255, blank=True, null=True)
+    show_all_page = PageField(
+        verbose_name=_('Show all - Page URL'), blank=True, null=True,
+        on_delete=models.SET_NULL,
+        help_text=_('A page has priority over an external URL'))
+    show_all_external = models.URLField(
+        _('Show all - External URL'), blank=True,
+        help_text=_('e.g. http://example.com/thank-you'))
 
     def __str__(self):  # __unicode__ on Python 2
         return self.title
@@ -22,12 +27,27 @@ class SpeLinkCategory(models.Model):
         verbose_name = "category"
         verbose_name_plural = "categories"
 
+    def get_absolute_url(self):
+        if self.show_all_page:
+            page = Page.objects.get(pk=self.show_all_page.id)
+            url = page.get_absolute_url()
+        else:
+            url = self.show_all_external
+        return url
+
+
 
 @python_2_unicode_compatible  # only if you need to support Python 2
 class SpeLink(models.Model):
     category = models.ForeignKey(SpeLinkCategory, on_delete=models.CASCADE, null=True)
     title = models.CharField(max_length=255, null=True)
-    url = models.URLField(max_length=255, blank=True, null=True)
+    page_url = PageField(
+        verbose_name=_('Page URL'), blank=True, null=True,
+        on_delete=models.SET_NULL,
+        help_text=_('A page has priority over an external URL'))
+    external_url = models.URLField(
+        _('External URL'), blank=True,
+        help_text=_('e.g. http://example.com/thank-you'))`
 
     def __str__(self):  # __unicode__ on Python 2
         return self.title
@@ -35,6 +55,14 @@ class SpeLink(models.Model):
     class Meta:
         verbose_name = "link"
         ordering = ['category__title', 'title']
+
+    def get_absolute_url(self):
+        if self.show_all_page:
+            page = Page.objects.get(pk=self.page_url.id)
+            url = page.get_absolute_url()
+        else:
+            url = self.external_url
+        return url
 
 
 @python_2_unicode_compatible
