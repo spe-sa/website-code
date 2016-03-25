1c39e03bf68abc9419d15fe092c9a5be2c4bda3d
jtoonyspe <jtoony@spe.org>
2016-03-25 07:13:15 -0500

    JD Sync with latest.

--- mainsite/settings/local.py
+++ mainsite/settings/local.py
@@ -15,28 +15,28 @@ CMS_TEMPLATES += (
 for logger in LOGGING['loggers']:
     LOGGING['loggers'][logger]['handlers'] = ['console']
 
-# DATABASES = {
-#     'default':
-#         {
-#             'ENGINE': 'django.db.backends.mysql', #.sqlite3
-#             'OPTIONS': {
-#                 'read_default_file': '/Applications/MAMP/tmp/mysql/my.cnf',
-#             },
-#             'NAME': 'website',
-#             'USER': 'root',
-#             'PASSWORD': 'root',
-#         }
-# }
-
 DATABASES = {
-    'default': {
-        'CONN_MAX_AGE': 0,
-        'ENGINE': 'django.db.backends.sqlite3',
-        'HOST': 'localhost',
-        'NAME': os.path.join(DATA_DIR, 'project.db'),
-        'PASSWORD': '',
-        'PORT': '',
-        'USER': ''
-    }
+    'default':
+        {
+            'ENGINE': 'django.db.backends.mysql',
+            'OPTIONS': {
+                'read_default_file': '/Applications/MAMP/tmp/mysql/my.cnf',
+            },
+            'NAME': 'website',
+            'USER': 'root',
+            'PASSWORD': 'root',
+        }
 }
+
+# DATABASES = {
+#     'default': {
+#         'CONN_MAX_AGE': 0,
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'HOST': 'localhost',
+#         'NAME': os.path.join(DATA_DIR, 'project.db'),
+#         'PASSWORD': '',
+#         'PORT': '',
+#         'USER': ''
+#     }
+# }
 
