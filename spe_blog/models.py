0d54d27b777331a622b93020ef10b055a3a6bdb6
jtoonyspe <jtoony@spe.org>
2016-05-23 12:55:56 -0500

    Misc fixes
    Wrapping issue in author box
    Special characters in Article Title breaking things.

--- spe_blog/models.py
+++ spe_blog/models.py
@@ -237,7 +237,8 @@ class Article(models.Model):
             buf = " " + str(self.print_volume)
         if self.print_issue:
             buf += ", " + str(self.print_issue)
-        buf = self.publication.code + buf + ": " + str(self.title)
+        # buf = self.publication.code + buf + ": " + str(self.title)
+        buf = self.publication.code + buf + ": " + unicode(self.title)
         if self.published:
             buf += " (PUBLISHED)"
         return buf
