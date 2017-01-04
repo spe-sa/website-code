955225fea1786ce9c6beedfa32bbaf1f6ce40b0e
bodenmemberships <jboden@jboden-mac.local>
2017-01-04 10:34:50 -0600

    Removing Old Promotions Table
    Cleanup only

--- spe_promotions/models.py
+++ spe_promotions/models.py
@@ -71,23 +71,23 @@ DISPLAY_TYPE = (
 #        return self.title
 
 
-class PromotionListingPlugin(CMSPlugin):
-    template = models.CharField(max_length=255, choices=PLUGIN_TEMPLATES, default=DEFAULT_PLUGIN_TEMPLATE)
-    count = models.PositiveIntegerField(default=5, verbose_name=u'Number of Promotions')
-    promotion_type = models.CharField(max_length=40, verbose_name="Promotion Type", choices=PROMOTION_TYPE,
-                                      default=DEFAULT_PROMOTION_TYPE)
-    disciplines = models.ManyToManyField(Tier1Discipline, blank=True, limit_choices_to={'active': True})
-    regions = models.ManyToManyField(Regions, blank=True)
-
-    def __unicode__(self):
-        buf = str(self.count) + " - " + self.promotion_type
-        buf += " (disciplines: %s)" % ', '.join([a.name for a in self.disciplines.all()])
-        buf += " (regions: %s)" % ', '.join([a.region_code for a in self.regions.all()])
-        return buf
-
-    def copy_relations(self, old_instance):
-        self.disciplines = old_instance.disciplines.all()
-        self.regions = old_instance.regions.all()
+# class PromotionListingPlugin(CMSPlugin):
+#     template = models.CharField(max_length=255, choices=PLUGIN_TEMPLATES, default=DEFAULT_PLUGIN_TEMPLATE)
+#     count = models.PositiveIntegerField(default=5, verbose_name=u'Number of Promotions')
+#     promotion_type = models.CharField(max_length=40, verbose_name="Promotion Type", choices=PROMOTION_TYPE,
+#                                       default=DEFAULT_PROMOTION_TYPE)
+#     disciplines = models.ManyToManyField(Tier1Discipline, blank=True, limit_choices_to={'active': True})
+#     regions = models.ManyToManyField(Regions, blank=True)
+#
+#     def __unicode__(self):
+#         buf = str(self.count) + " - " + self.promotion_type
+#         buf += " (disciplines: %s)" % ', '.join([a.name for a in self.disciplines.all()])
+#         buf += " (regions: %s)" % ', '.join([a.region_code for a in self.regions.all()])
+#         return buf
+#
+#     def copy_relations(self, old_instance):
+#         self.disciplines = old_instance.disciplines.all()
+#         self.regions = old_instance.regions.all()
 
 
 class SimpleEventPromotion(models.Model):
