dc25a8d5fc46bf3048ee6c96f1e36c8e00c7f007
bodenmemberships <jboden@jboden-mac.local>
2017-01-11 14:11:23 -0600

    Change All Default Regions to Be Lookup

--- mainsite/common.py
+++ mainsite/common.py
@@ -8,7 +8,7 @@ def getRegion(context, fallback_region):
     # ip = context['request'].META.get('REMOTE_ADDR', None)
     # ip = context['request'].META.get('HTTP_X_REAL_IP', None)
     ip = get_ip(context['request'])
-    region = Web_Region_Country.objects.get(country_UN=fallback_region).region
+    region = fallback_region
     if ip:
         loc = g.city(ip)
         if loc:
@@ -16,5 +16,5 @@ def getRegion(context, fallback_region):
             try:
                 region = Web_Region_Country.objects.get(country_UN=country).region
             except Web_Region_Country.DoesNotExist:
-                region = Web_Region_Country.objects.get(country_UN=fallback_region).region
+                region = fallback_region
     return region
