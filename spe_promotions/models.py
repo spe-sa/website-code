69b8f8b63afe17e797206c129b94a985d191345f
bodenmemberships <jboden@jboden-mac.local>
2017-01-12 15:11:21 -0600

    SPE Promotion Cleanup of Look and Feel

--- spe_promotions/models.py
+++ spe_promotions/models.py
@@ -1,8 +1,6 @@
 import datetime
 
 from django.db import models
-from django.utils import timezone
-from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 from cms.models import CMSPlugin
