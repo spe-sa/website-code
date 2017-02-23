4c2dc55b385dfa214573fa810ea377d5e383153e
Steve Stacha <sstacha@gmail.com>
2017-02-23 17:11:29 -0600

    commiting code that seems to work thus far.  Testing tomorrow and doing QA rounds.

--- mainsite/management/commands/images.py
+++ mainsite/management/commands/images.py
@@ -1,5 +1,7 @@
 from django.core.management.base import BaseCommand, CommandError
 from filer.models.filemodels import File
+from shutil import copyfile
+import os, errno
 
 
 class Command(BaseCommand):
@@ -23,6 +25,28 @@ class Command(BaseCommand):
             help='List Duplicate Files in Filer',
         )
 
+        parser.add_argument(
+            '--transfer',
+            action='store_true',
+            dest='transfer',
+            default=False,
+            help='Transfer logical filer directories to physical directories [images/<filer-dir>/<img_name>]; NOTE: will replace any existing files',
+        )
+
+    def get_altered_path(self, p):
+        p.lower()
+        p.replace(" ", "_")
+        return p
+
+    def mkdir_p(self, path):
+        try:
+            os.makedirs(path)
+        except OSError as exc:  # Python >2.5
+            if exc.errno == errno.EEXIST and os.path.isdir(path):
+                pass
+            else:
+                raise
+
     def handle(self, *args, **options):
         images = File.objects.all()
         if options['list']:
@@ -44,3 +68,45 @@ class Command(BaseCommand):
                         self.stdout.write(buf)
                         buf = "\t" + str(i) + ". File Path:    " + dup.path
                         self.stdout.write(buf)
+        if options['transfer']:
+            x=0
+            docbase = "/vagrant/ubuntu/djangocms/website_content/media"
+            imgbase = "/vagrant/ubuntu/djangocms/website_content/new_media"
+            self.stdout.write("images: %s" % len(images))
+            for image in images:
+                x=x+1
+                # self.stdout.write("***** original_file: %s" % image.original_filename)
+                # self.stdout.write("***** file: %s" % image.file)
+                f = image.file
+                f = '/' + str(f).lower()
+                p = '/' + "/".join([_f.name for _f in image.logical_path + [image]])
+
+                # self.stdout.write("##### filename: %s" % image.name)
+                # self.stdout.write("***** logical path: %s" % p)
+                ap = p.lower().replace(" ", "_")
+                # self.stdout.write("***** altered path: %s" % ap)
+                if len(image.name) > 0:
+                    pos = len(ap) - len(image.name)
+                    if pos >= 1:
+                        ap = ap[0:pos]
+                # self.stdout.write("***** altered path after imagename: %s" % ap)
+                # self.stdout.write("making directory if it doesn't exist: %s" % imgbase + ap)
+                self.mkdir_p(imgbase + ap)
+                nf = ap + image.original_filename.lower()
+                # self.stdout.write("***** altered path: %s" % nf)
+                # buf = image.path + "\t" + image.original_filename + "\t" + str(image.owner) + "\t" + str(image.size)
+                self.stdout.write("%s -> %s [%s]" % (f, nf, image.size))
+                # self.stdout.write("file: %s" % f)
+                # src = docbase + image.file
+                # dst = imgbase + nf
+                # copyfile(src, dst)
+                # for attr in image.__dict__:
+                #     self.stdout.write("obj.%s = %s" % (attr, getattr(image, attr)))
+                src = docbase + f
+                dst = imgbase + nf
+                # self.stdout.write("src: %s" % src)
+                # self.stdout.write("dst: %s" % dst)
+                copyfile(src, dst)
+
+                if x>10:
+                    break
