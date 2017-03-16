18b00009112e6c9cc635cbd6daa67652e2e72174
bodenmemberships <jboden@jboden-mac.local>
2017-03-16 16:52:46 -0500

    Settings File for QA and PROD version of Reporting Server

---
+++ mainsite/settings/sitereportsprod.py
@@ -0,0 +1,19 @@
+#from __future__ import absolute_import # optional, but I like it
+from .common import *
+
+# Development overrides
+DEBUG = False
+ENVIRONMENT = "PROD"
+WS_EVENTS_URL = 'http://iisprod1/iappsint/p13ndemo/api/I2KTaxonomy/GetEventList3'
+WS_EVENTS_GENERIC_URL = 'http://iisprod1/iappsint/p13ndemo/api/I2KTaxonomy/GetEventList4'
+
+# make all loggers use the file & make sure debugging is turned to warning.
+for logger in LOGGING['loggers']:
+    # LOGGING['loggers'][logger]['handlers'] = ['file']
+    if LOGGING['loggers'][logger]['level'] == 'DEBUG':
+        LOGGING['loggers'][logger]['level'] = 'ERROR'
+
+# tuple for old host to new host for this environment
+HOST_REPLACEMENTS = (
+    ('www.spe.org', 'sitereportsprod.spe.org'),
+)
