1763039690e4b133751a00f90cb38799ce7941d0
bodenmemberships <jboden@jboden-mac.local>
2017-04-05 14:09:02 -0500

    Delete Backup Folder

--- .spe_preferences.backup_70307-0918/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import PreferenceGroup, Preference, CustomerPreference
-
-
-admin.site.register(PreferenceGroup)
-admin.site.register(Preference)
-admin.site.register(CustomerPreference)
