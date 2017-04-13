2cfa89f7a96bb49a4a3921b9046fce64dbc438ff
Brett Fountain <bfountain@spe.org>
2017-04-13 14:01:37 -0500

    simplifying the regex pointing to contact_prefs

--- spe_preferences/urls.py
+++ spe_preferences/urls.py
@@ -6,7 +6,8 @@ from . import views
 urlpatterns = [
     url(r'^myip/?$', views.myip, name="myip"),
     #url(r'^comm_prefs/?$', views.comm_prefs, name='comm'),
-    url(r'^co(?:ntact|mm)(?:(?:unication)?s?_pref(?:erence?)?s?)?/?$', views.contact_prefs, name='contact'),
+#    url(r'^co(?:ntact|mm)(?:(?:unication)?s?_pref(?:erence?)?s?)?/?$', views.contact_prefs, name='contact'),
+    url(r'^contact_prefs/?$', views.contact_prefs, name='contact'),
 
     url(r'^who/(?P<constituent_id_provided>ci:[0-9a-zA-Z]+)?(?P<email_address_provided>ea:[0-9a-zA-Z_.-]+@[0-9a-zA-Z_.-]+\.[0-9a-zA-Z]+)?$', views.quick_find_user, name='who'),
     url(r'^allow/(?P<preference_code>[a-z0-9A-Z]+):(?P<yes_or_no>[YyEeSsNnOoTtRrUuFfAaLl10]+)$', views.allow_communication_for, name='allow'),
