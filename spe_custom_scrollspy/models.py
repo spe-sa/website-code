fdd3eb51aa203d8f24655df282316484ebb57318
bodenmemberships <jboden@jboden-mac.local>
2017-05-12 13:36:23 -0500

    SPE Custom ScrollSpy
    Cleanup

--- spe_custom_scrollspy/models.py
+++ spe_custom_scrollspy/models.py
@@ -22,7 +22,7 @@ class ScrollSpy(CMSPlugin):
     title = models.CharField(max_length=64)
 
     def __unicode__(self):
-        return u"{0}".format(self.title)
+        return u"{0} - {1} scrollspy items".format(self.title, self.cmsplugin_set.all().count())
 
 
 
