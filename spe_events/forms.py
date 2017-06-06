a05cf9f6aabefa203ccd9e24100764fff486b1ba
jtoonyspe <jtoony@spe.org>
2017-06-06 08:16:45 -0500

    ATCE fixes

--- spe_events/forms.py
+++ spe_events/forms.py
@@ -4,7 +4,7 @@ from .models import ImageItems
 
 class ImageItemInLineForm(forms.ModelForm):
 
-    imageurl = forms.CharField(widget=forms.TextInput(attrs={'data-validate-url': 'true'}))
+    imageurl = forms.CharField(widget=forms.TextInput(attrs={'data-validate-url': 'true'}), required=False)
 
     class Meta:
         model = ImageItems
